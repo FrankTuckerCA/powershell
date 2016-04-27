5b65f5e5e526d8ff68f5c9d98266077da6e8396f
FrankTuckerCA <franktucker@LanSol.onmicrosoft.com>
2016-04-27 13:44:21 -0700

    Find SMB Shares on computers
    Find and Report SMB Shares on computers. Find 'rouge' shares.

---
+++ AmazingChartsBackupCleanup.ps1
@@ -0,0 +1,41 @@
+﻿#Amazing Charts, Remove Old backup files.
+#FrankTucker@lan-sol.com
+#AC runs a daily backup to program folder (Cannot Change). The backup file can be large, 25GB Plus.
+#Only remove files if more than 7 total backup files AND the backup from yesturday is compelete. 
+#Removes backups 8 days and older
+
+#Dates
+$Date = Get-Date
+$LastWeek = ($date.AddDays(-7))
+$Yesturday = ($date.AddDays(-1))
+
+#Amazing Charts Backup Folder
+$BackupFolder  = ('C:\Program Files (x86)\Amazing Charts\Backup\')
+Set-Location $BackupFolder
+
+
+#File enc Backup files older then 7 Days
+$oldfiles = 0,0
+$oldfiles = (Get-ChildItem -Path $BackupFolder -Filter *enc | Where-Object -Property CreationTime -LT $LastWeek)
+
+#Lenghth and Sum/Size of all enc backup files
+$TotalBackupInfo = (Get-ChildItem -Path $BackupFolder -Filter *enc | measure -Property Length -sum )
+
+#Length and Sum/Size of yesturday backup file
+$YesturdayBackupInfo = ( Get-ChildItem -Path $BackupFolder -Filter *enc | where-Object -Property CreationTime -GT $yesturday | measure -Property Length -sum )
+
+
+#Check the number and Sum/Size of backup files, remove files older than 7 days if sum and number are correct.
+
+if ($YesturdayBackupInfo.Sum -gt 26000000000 -and $TotalBackupInfo.Count -gt 7 -and $TotalBackupInfo.sum -gt 182000000000)
+{
+   
+ remove-item $oldfiles -force
+      
+}
+
+
+
+
+
+
---
+++ Find-SMBShare/Find-SMBShare.psm1
@@ -0,0 +1,101 @@
+﻿Function Find-SMBShare {
+ 
+#franktucker@lan-sol.com
+#requires -runasadministrator 
+#requires -version 3.0 
+
+<#
+	.SYNOPSIS
+		 Function searchs for shared items on a computer/s and reports
+	.EXAMPLE
+		PS> find-smbshare -computername workstation1 -credential domain\username
+
+        ComputerName  : workstation1
+        Status        : Connected
+        TotalShare    : 8
+        AdminShare    : {ADMIN$, C$, E$...}
+        PrinterShare  : Brother7840BW, CannonColor
+        NotAdminFound : True
+        NotAdminShare : DDrive
+	
+		This example finds shared items on \\workstation1.
+    .EXAMPLE
+        PS> $credential = (get-credential)
+        PS> find-smbshare -computername server -credential $credential
+        
+        This example finds shared items on \\server with a saved pscredential
+    .PARAMETER computername
+		The computer/s to search 
+	.PARAMETER credential
+		Administrative account on computer/s
+        Can be a [string]username or [object]pscredential
+	#>
+
+[cmdletBinding()]
+param(
+        [Parameter(Mandatory=$False,
+                   ValueFromPipeline=$True,
+                   ValueFromPipelineByPropertyName=$True,
+                   HelpMessage="The. Computer. Name.")]
+        [Alias('Hostname','cn')]
+        [string[]]$ComputerName,
+
+        [parameter(Mandatory=$True,
+                    ValueFromPipeLine=$True,
+                    ValueFromPipelineByPropertyName=$True,
+                    HelpMessage="Admin. Username. Or. PSCredential")]
+        [Alias('user','username','pscredential')]
+        [object]$Credential      
+)   
+
+       
+if ($Credential.ToString() -contains 'System.Management.Automation.PSCredential'){
+        $pscredential = $Credential}
+    else{
+        Write-Verbose "getting credential"
+        $pscredential = (Get-Credential -Credential $Credential)}  
+
+foreach ($computer in $computername){
+    try {
+        Write-Verbose "Connecting to $computer"
+        $Session = New-CimSession -ComputerName $Computer -Credential $pscredential -ErrorAction Stop 
+        Write-Verbose "Get Data from $computer"        
+        $Share = ( Get-CimSession -InstanceId $Session.InstanceId | Get-CimInstance -ClassName Win32_Share)
+        $PrinterShare = ($Share | Where-Object {$_.name -notlike '*$*'} | Where-Object {$_.path -notlike '*:\*'})
+        $AdminShare = ($share | Where-Object {$_.type -ne 0 } | Where-Object {$_.name -like '*$' } )
+        $NotAdminShare = ($share | Where-Object {$_.type -eq 0} |  Where-Object {$_.name -notlike '*$'} )               
+        $properties = [ordered]@{ComputerName = $computer
+                        Status = 'Connected'
+                        TotalShare = $share.count
+                        AdminShare = $adminShare.name
+                        PrinterShare = $printershare.name
+                        NotAdminFound = [boolean]$NotAdminShare
+                        NotAdminShare = $notadminshare.name}
+        Remove-CimSession -instanceID $Session.InstanceId
+
+        } catch {
+            Write-Verbose "Couldn't connect to $computer"
+            Write-Verbose "Check Credential"
+            $properties = [ordered]@{ComputerName = $comptuer
+                        Status = 'Disconnected'
+                        TotalShare = $null
+                        AdminShare = $null
+                        PrinterShare = $null
+                        NotAdminFound = [boolean]$null
+                        NotAdminShare = $null}
+        } finally {
+            $obj = New-Object -TypeName PSObject -Property $properties
+            Write-Output $obj
+        }
+    }
+}
+
+                        
+                        
+                        
+
+
+
+
+
+
