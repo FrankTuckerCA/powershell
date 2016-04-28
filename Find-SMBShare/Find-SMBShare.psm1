e3500be5f416f5c62968275af98d3a09568ff980
FrankTuckerCA <FrankTucker@lan-sol.com>
2016-04-28 10:38:32 -0700

    Update Find-SMBShare.psm1

--- Find-SMBShare/Find-SMBShare.psm1
+++ Find-SMBShare/Find-SMBShare.psm1
@@ -1,16 +1,16 @@
-﻿Function Find-SMBShare {
- 
-#franktucker@lan-sol.com
-#requires -runasadministrator 
-#requires -version 3.0 
+Function Find-SMBShare 
+{
+    #FrankTucker@lan-sol.com
+    #Requires -Runasadministrator 
+    #Requires -Version 3.0 
 
-<#
+    <#
 	.SYNOPSIS
 		 Function searchs for shared items on a computer/s and reports
 	.EXAMPLE
-		PS> find-smbshare -computername workstation1 -credential domain\username
+		PS> Find-SMBShare -Computername Workstation1 -Credential domain\username
 
-        ComputerName  : workstation1
+        ComputerName  : Workstation1
         Status        : Connected
         TotalShare    : 8
         AdminShare    : {ADMIN$, C$, E$...}
@@ -18,21 +18,25 @@
         NotAdminFound : True
         NotAdminShare : DDrive
 	
-		This example finds shared items on \\workstation1.
+		This example finds shared items on \\Workstation1.
     .EXAMPLE
-        PS> $credential = (get-credential)
-        PS> find-smbshare -computername server -credential $credential
+        PS> $Credential = (Get-Credential)
+        PS> Find-SMBShare -Computername Server,Workstation1 -Credential $Credential
         
-        This example finds shared items on \\server with a saved pscredential
+        This example finds shared items on \\Server and \\Workstation1 with a saved PSCredential
+    .EXAMPLE
+        PS> Find-SMBShare -Computername Server
+        
+        This example finds shared items on \\Server with the current logon user credential.   
     .PARAMETER computername
 		The computer/s to search 
 	.PARAMETER credential
 		Administrative account on computer/s
         Can be a [string]username or [object]pscredential
-	#>
+    #>
 
-[cmdletBinding()]
-param(
+    [cmdletBinding()]
+    param(
         [Parameter(Mandatory=$False,
                    ValueFromPipeline=$True,
                    ValueFromPipelineByPropertyName=$True,
@@ -40,40 +44,67 @@ param(
         [Alias('Hostname','cn')]
         [string[]]$ComputerName,
 
-        [parameter(Mandatory=$True,
-                    ValueFromPipeLine=$True,
+        [parameter(Mandatory=$False,
+                    ValueFromPipeLine=$False,
                     ValueFromPipelineByPropertyName=$True,
                     HelpMessage="Admin. Username. Or. PSCredential")]
         [Alias('user','username','pscredential')]
         [object]$Credential      
-)   
+    )   
+    Write-Verbose "Checking Credential Param"
+    if  ($Credential.count -ne 0)
+    {       
+        if ($Credential.ToString() -contains 'System.Management.Automation.PSCredential')
+        {
+            $PSCredential = $Credential           
+        }
+         else
+        {
+        Write-Verbose "Getting Credential"
+        $PSCredential = (Get-Credential -Credential $Credential)       
+        }
+      
+     }
+     Write-Verbose "Checking ComputeName Param"
+     if ($ComputerName.count -eq 0)
+     {
+     $ComputerName = $env:COMPUTERNAME
+     }
 
-       
-if ($Credential.ToString() -contains 'System.Management.Automation.PSCredential'){
-        $pscredential = $Credential}
-    else{
-        Write-Verbose "getting credential"
-        $pscredential = (Get-Credential -Credential $Credential)}  
+    foreach ($computer in $computername)
+    {
+        try
+        {
+            Write-Verbose "Connecting to $computer"
+            
+                if ($PSCredential.count -ne 0)
+                {
+                $Session = New-CimSession -ComputerName $Computer -Credential $PSCredential -ErrorAction Stop
+                }
+                else
+                {
+                $Session = New-CimSession -ComputerName $computer -ErrorAction stop
+                }
 
-foreach ($computer in $computername){
-    try {
-        Write-Verbose "Connecting to $computer"
-        $Session = New-CimSession -ComputerName $Computer -Credential $pscredential -ErrorAction Stop 
-        Write-Verbose "Get Data from $computer"        
-        $Share = ( Get-CimSession -InstanceId $Session.InstanceId | Get-CimInstance -ClassName Win32_Share)
-        $PrinterShare = ($Share | Where-Object {$_.name -notlike '*$*'} | Where-Object {$_.path -notlike '*:\*'})
-        $AdminShare = ($share | Where-Object {$_.type -ne 0 } | Where-Object {$_.name -like '*$' } )
-        $NotAdminShare = ($share | Where-Object {$_.type -eq 0} |  Where-Object {$_.name -notlike '*$'} )               
-        $properties = [ordered]@{ComputerName = $computer
+            Write-Verbose "Getting Data from $computer"   
+                 
+            $Share = ( Get-CimSession -InstanceId $Session.InstanceId | Get-CimInstance -ClassName Win32_Share)
+            $PrinterShare = ($Share | Where-Object {$_.name -notlike '*$*'} | Where-Object {$_.path -notlike '*:\*'})
+            $AdminShare = ($share | Where-Object {$_.type -ne 0 } | Where-Object {$_.name -like '*$' } )
+            $NotAdminShare = ($share | Where-Object {$_.type -eq 0} |  Where-Object {$_.name -notlike '*$'} )               
+
+            $Properties = [ordered]@{ComputerName = $computer
                         Status = 'Connected'
                         TotalShare = $share.count
                         AdminShare = $adminShare.name
                         PrinterShare = $printershare.name
                         NotAdminFound = [boolean]$NotAdminShare
                         NotAdminShare = $notadminshare.name}
-        Remove-CimSession -instanceID $Session.InstanceId
 
-        } catch {
+        Remove-CimSession -instanceID $Session.InstanceId
+        } 
+        catch
+        {
             Write-Verbose "Couldn't connect to $computer"
             Write-Verbose "Check Credential"
             $properties = [ordered]@{ComputerName = $comptuer
@@ -83,13 +114,14 @@ foreach ($computer in $computername){
                         PrinterShare = $null
                         NotAdminFound = [boolean]$null
                         NotAdminShare = $null}
-        } finally {
-            $obj = New-Object -TypeName PSObject -Property $properties
-            Write-Output $obj
+        } 
+        finally
+        {
+            $Obj = New-Object -TypeName PSObject -Property $properties
+            Write-Output $Obj
         }
     }
 }
-
                         
                         
                         
